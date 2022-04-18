9f0f06c7c5514fb53a82bd676e31966c928c6a16
nahtandg <nathan.silva@padolabs.com.br>
2022-04-18 17:00:39 -0300

    add array and delete

--- src/app.js
+++ src/app.js
@@ -7,14 +7,22 @@ const port = 4002;
 app.use(cors());
 app.use(json());
 
+let moistures = [];
+
 app.get('/', (req, res) => {
-    res.send('hello world!')
+    res.send(`moisture => ${moistures}`)
 })
 
 app.post('/', (req, res) => {
     const request = req.body;
+    moistures.push(request.moisture);
     console.log(request);
     res.status(201).send();
+})
+
+app.delete('/', (req, res) => {
+    moistures = [];
+    moistures.status(200).send();
 })
 
 app.listen(port, (req, res) => {
