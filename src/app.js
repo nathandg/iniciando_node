c7b442ae09ddd69703e6b4039db170fd5cb5cc9e
nahtandg <nathan.silva@padolabs.com.br>
2022-04-18 16:37:32 -0300

    add-cors-and-json

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
+const { json } = require('body-parser');
 const port = 4002;
+
+app.use(cors());
+app.use(json());
 
 app.get('/', (req, res) => {
     res.send('hello world!')
