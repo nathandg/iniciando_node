c1a95db06bf9d16f0d8c441099342d02d5d2a042
nathan <nathan.silva@padolabs.com.br>
2022-04-23 14:07:50 -0300

    add post and query parameter

--- src/app.js
+++ src/app.js
@@ -7,22 +7,46 @@ const port = 4002;
 app.use(cors());
 app.use(json());
 
+let dummyCount = 0;
 let moistures = [];
 
 app.get('/', (req, res) => {
-    res.send(`moistures: ${moistures}`)
+    res.send(moistures);
 })
 
 app.post('/', (req, res) => {
+    
     const request = req.body;
-    moistures.push(request.moisture);
-    console.log(request);
+    
+    //created obj
+    const moisturesObj = {
+
+        id: dummyCount += 1,
+        moisture: request.moisture
+    }
+    console.log(moisturesObj);
+    moistures.push(moisturesObj);
     res.status(201).send();
 })
 
 app.delete('/', (req, res) => {
     moistures = [];
     res.status(200).send();
+})
+
+app.put('/:value', (req, res) => {
+    const value = req.params.value
+    const id = req.query.id
+    console.log(`QUERY IS ${id} AND PARAMETER IS ${value}`)
+    
+    moistures.map(moisture => {
+         if(moisture.id == id){
+             console.log(`FOUND ID ${id} CHANGING VALUE OF OBJECT`)
+             moisture.moisture = parseInt(value);
+         }
+     });
+
+    res.status(200).send()
 })
 
 app.listen(port, (req, res) => {
