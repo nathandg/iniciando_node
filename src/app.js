8df2cca8f634a392a8fbe47662319ced62056e78
nahtandg <nathan.silva@padolabs.com.br>
2022-04-25 15:53:08 -0300

    show html files in get ('/')

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { json } = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 
 const server = express()
 const port = 4002
@@ -10,6 +11,10 @@ const moistureRoute = require('./routes/moistures')
 server.use(json());
 server.use(cors());
 server.use('/moistures', moistureRoute);
+
+server.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'views', 'index.html'))
+})
 
 server.listen(port, () => {
     console.log(`server running on port ${port}`)
